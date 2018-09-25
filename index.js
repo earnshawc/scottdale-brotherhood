6cb29634738d628fc2b280559027f96e3d79d981
Artemka076 <artemkamasatka@mail.ru>
2018-09-25 21:12:51 +0300

    Add files via upload

--- index.js
+++ index.js
@@ -217,6 +217,7 @@ bot.on('message', async message => {
     if (message.guild.id != "355656045600964609" && message.guild.id != "488400983496458260" && message.guild.id != "493459379878625320") return
     if (message.type === "PINS_ADD") if (message.channel.name == "requests-for-roles") message.delete();
     if (message.content == "/ping") return message.reply("`я онлайн.`") && console.log(`Бот ответил ${message.member.displayName}, что я онлайн.`)
+    if (message.member.id == bot.user.id) return
 
     if (message.guild.id == 488400983496458260){
         if (message.channel.name == "ваши-предложения"){
