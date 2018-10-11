b01b4b5aef0f0db58b153ae4d7ad4e6e82157806
Artemka076 <artemkamasatka@mail.ru>
2018-10-11 19:22:06 +0300

    Add files via upload

--- index.js
+++ index.js
@@ -670,13 +670,12 @@ bot.on('message', async message => {
         })
         const embed = new Discord.RichEmbed()
         .setColor("#FF0000")
-        .setFooter(`Информация предоставлена: ${message.member.displayName}`)
+        .setFooter(`Аккаунт пользователя: ${user.displayName}`, user.user.avatarURL)
         .setTimestamp()
-        .setThumbnail(user.user.defaultAvatarURL)
+        .addField(`Дата создания аккаунта и входа на сервер`, `**Аккаунт создан:** \`${user.user.createdAt.getFullYear()}-${user.user.createdAt.getMonth()}-${user.user.createdAt.getDay()} ${user.user.createdAt.getHours()}:${user.user.createdAt.getMinutes()}:${user.user.createdAt.getSeconds()}\`\n**Вошел к нам:** \`${user.joinedAt.getFullYear()}-${user.joinedAt.getMonth()}-${user.joinedAt.getDay()} ${user.joinedAt.getHours()}:${user.joinedAt.getMinutes()}:${user.joinedAt.getSeconds()}\``)
         .addField("Информация", 
         `**Аккаунт создан:** ${user.user.createdAt.getFullYear()}\n` +
-        `**Роли:** ${userroles}\n` +
-        `**Permissions:** ${user.permissions.toArray()}`)
+        `**Роли:** ${userroles}`)
         message.reply(`**вот информация по поводу аккаунта <@${user.id}>**`, embed)
     }
 
