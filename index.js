423c38a29c9569f22ebc192b7d48095e811c2a84
Artemka076 <artemkamasatka@mail.ru>
2018-10-20 19:32:56 +0300

    Update index.js

--- index.js
+++ index.js
@@ -200,6 +200,12 @@ function checknick (member, role, startnum, endnum, bot, message){
     }
 }
 
+function getRandomInt(min, max) {
+    min = Math.ceil(min);
+    max = Math.floor(max + 1);
+    return Math.floor(Math.random() * (max - min)) + min;
+}
+
 function hook(channel, name, message, avatar) {
 
     if (!channel) return console.log('Channel not specified.');
