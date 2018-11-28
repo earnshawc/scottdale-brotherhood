71d7fa71dd02517af4680b985fe2f840b30a3f3c
Artemka076 <artemkamasatka@mail.ru>
2018-11-28 18:17:21 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-const Discord = require('discord.js');
+const Discord = require('discord.js'); // by Artemka076#6715
 const bot = new Discord.Client();
 const fs = require("fs");
 const Logger = require('./objects/logger');
