60718eed465ed9081fdc9e7fa88afa227df8e167
Artemka076 <artemkamasatka@mail.ru>
2018-11-29 21:56:52 +0300

    Update index.js

--- index.js
+++ index.js
@@ -541,7 +541,7 @@ if (message.content == '/toadmin'){
     ADD_REACTIONS: false,
   })  
 
-  await channel.overwritePermissions(message.guild.roles.find(r => r.name.includes('Administrator')), {
+  await channel.overwritePermissions(message.guild.roles.find(r => r.name.includes('Administrator'), {
     // GENERAL PERMISSIONS
     CREATE_INSTANT_INVITE: false,
     MANAGE_CHANNELS: false,
@@ -560,7 +560,7 @@ if (message.content == '/toadmin'){
     ADD_REACTIONS: false,
   })  
 
-  await channel.overwritePermissions(message.guild.roles.find(r => r.name.includes('Jr.Administrator')), {
+  await channel.overwritePermissions(message.guild.roles.find(r => r.name.includes('Jr.Administrator'), {
     // GENERAL PERMISSIONS
     CREATE_INSTANT_INVITE: false,
     MANAGE_CHANNELS: false,
@@ -579,7 +579,7 @@ if (message.content == '/toadmin'){
     ADD_REACTIONS: false,
   })  
 
-  await channel.overwritePermissions(message.guild.roles.find(r => r.name.includes('Helper')), {
+  await channel.overwritePermissions(message.guild.roles.find(r => r.name.includes('Helper'), {
     // GENERAL PERMISSIONS
     CREATE_INSTANT_INVITE: false,
     MANAGE_CHANNELS: false,
