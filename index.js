68f04b3032deea90ac67de9d90f3a782215c45e6
Artemka076 <artemkamasatka@mail.ru>
2018-12-12 19:17:26 +0300

    Update index.js

--- index.js
+++ index.js
@@ -257,7 +257,7 @@ bot.on('message', async message => {
     if (message.guild.id != serverid && message.guild.id != "493459379878625320") return
     if (message.type === "PINS_ADD") if (message.channel.name == "requests-for-roles") message.delete();
     if (message.content == "/ping") return message.reply("`я онлайн!`") && console.log(`Бот ответил ${message.member.displayName}, что я онлайн.`)
-    if (message.member.id == bot.user.id) return
+    if (message.author.id == bot.user.id) return
     
     let re = /(\d+(\.\d)*)/i;
     
