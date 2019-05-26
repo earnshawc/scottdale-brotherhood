8e4c3ab03bbb8a3773d7b0ee53f59d164ff8db46
Yuki Flores <45567338+YukiFlores@users.noreply.github.com>
2019-05-26 23:48:29 +0300

    Update dsponts.js

--- global_systems/dsponts.js
+++ global_systems/dsponts.js
@@ -133,12 +133,14 @@ exports.run = async (bot, message, ds_cooldown, connection, mysql_cooldown, send
                     connection.query(`UPDATE \`profiles\` SET money = money - ${+args[2]} WHERE \`user\` = '${message.author.id}' AND \`server\` = '${message.guild.id}'`);
                     connection.query(`INSERT INTO \`profiles\` (\`server\`, \`user\`, \`money\`) VALUES ('${message.guild.id}', '${user.id}', '${+args[2]}')`);
                     send_action(message.guild.id, `<@${message.author.id}> перевел ${+args[2]} dp пользователю <@${user.id}> (${+result[0].money - +args[2]}-${+args[2]})`);
+                    send_action(message.guild.id, `<@${user.id}> получил от <@${message.author.id}> | ${+args[2]} dp (0-${+answer[0].money + +args[2]})`);
                     await message.reply(`**\`вы успешно передали ${args[2]} dp пользователю\` ${user}**`);
                     return message.delete();
                 }else{
                     connection.query(`UPDATE \`profiles\` SET money = money - ${+args[2]} WHERE \`user\` = '${message.author.id}' AND \`server\` = '${message.guild.id}'`);
                     connection.query(`UPDATE \`profiles\` SET money = money + ${+args[2]} WHERE \`user\` = '${user.id}' AND \`server\` = '${message.guild.id}'`);
                     send_action(message.guild.id, `<@${message.author.id}> перевел ${+args[2]} dp пользователю <@${user.id}> (${+result[0].money - +args[2]}-${+answer[0].money + +args[2]})`);
+                    send_action(message.guild.id, `<@${user.id}> получил от <@${message.author.id}> | ${+args[2]} dp (${+answer[0].money}-${+answer[0].money + +args[2]})`);
                     await message.reply(`**\`вы успешно передали ${args[2]} dp пользователю\` ${user}**`);
                     return message.delete();
                 }
