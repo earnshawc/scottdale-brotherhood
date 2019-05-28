224cb3511ebde266d1b849c5175e7a819eb69eb4
Kory McGregor <artemka076.dimaneal@gmail.com>
2019-05-29 02:01:49 +0300

    Update dsponts.js

--- global_systems/dsponts.js
+++ global_systems/dsponts.js
@@ -30,31 +30,25 @@ function uses(message, command, uses_args, settings_args){
         return true;
     }
     for (let i = 0; i <= settings_args.length; i++){
-        console.log(settings_args[i]);
-        console.log(settings_args[i] == 'number');
         if (settings_args[i] == 'number'){
             if (!isNumeric(args[+i + 1])){
-                console.log('no numeretic')
                 message.reply(`**\`использование: ${command} [${uses_args.join('] [')}]\nError: значение '${uses_args[i]}' не является числом.\`**`).then(msg => msg.delete(12000));
                 message.delete();
                 return true;
             }
         }else if (settings_args[i] == 'integer'){
             if (!isNumeric(args[+i + 1])){
-                console.log('no numeretic')
                 message.reply(`**\`использование: ${command} [${uses_args.join('] [')}]\nError: значение '${uses_args[i]}' не является числом.\`**`).then(msg => msg.delete(12000));
                 message.delete();
                 return true;
             }
-            if (!isInteger(args[+i + 1])){
-                console.log('no integer')
+            if (!isInteger(+args[+i + 1])){
                 message.reply(`**\`использование: ${command} [${uses_args.join('] [')}]\nError: значение '${uses_args[i]}' не целое.\`**`).then(msg => msg.delete(12000));
                 message.delete();
                 return true;
             }
         }
     }
-    console.log('end')
     return false;
 }
 
