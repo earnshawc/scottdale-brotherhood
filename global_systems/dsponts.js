49708130fdeadb8f29636946fd07250b53bc54f6
Kory McGregor <artemka076.dimaneal@gmail.com>
2019-05-30 11:17:04 +0300

    Update dsponts.js

--- global_systems/dsponts.js
+++ global_systems/dsponts.js
@@ -19,17 +19,14 @@ function error_mysql(error, message){
 }
 
 function time(s) {
-    function pad(n, z) {
-      z = z || 2;
-      return ('00' + n).slice(-z);
-    }
     var ms = s % 1000;
     s = (s - ms) / 1000;
     var secs = s % 60;
     s = (s - secs) / 60;
     var mins = s % 60;
     var hrs = (s - mins) / 60;
-    return pad(hrs) + ':' + pad(mins) + ':' + pad(secs) + '.' + pad(ms, 3);
+  
+    return hrs + ' часов ' + mins + ' минут ' + secs + '.' + ms + ' секунд';
 }
 
 function mysql_load(message, mysql_cooldown){
